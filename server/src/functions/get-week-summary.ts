a2bcc92368aea6a1f6375976722c3dfbf732b5bd
caiomarcelo79 <caio.marcelo79@gmail.com>
2024-09-13 21:51:56 -0300

    Update

--- server/src/functions/get-week-summary.ts
+++ server/src/functions/get-week-summary.ts
@@ -59,7 +59,22 @@ export async function getWeekSummary() {
 
   const result = await db
     .with(goalsCreatedUpToWeek, goalsCompletedInWeek, goalsCompletedByWeekDay)
-    .select()
+    .select({
+      completed:
+        sql /*sql*/`(SELECT COUNT(*) FROM ${goalsCompletedInWeek})`.mapWith(
+          Number
+        ),
+      total:
+        sql /*sql*/`(SELECT SUM(${goalsCreatedUpToWeek.desiredWeeklyFrequency}) FROM ${goalsCreatedUpToWeek})`.mapWith(
+          Number
+        ),
+      goalsPerDay: sql /*sql*/`
+        JSON_OBJECT_AGG(
+          ${goalsCompletedByWeekDay.completedAtDate},
+          ${goalsCompletedByWeekDay.completions}
+        )
+      `,
+    })
     .from(goalsCompletedByWeekDay)
 
   return {
